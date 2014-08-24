109a816d4662c656ce24d862befe8e16635249df
weimuhua <weimuhua0812@126.com>
2014-08-24 17:10:30 +0800

    一些简单的排序算法的实现。

---
+++ Sort/Sort.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int arr[] = {98,76,109,34,67,190,80,12,14,89,1,23,33,65,567,233,190,12};
+int len = 18;
+
+void pri(int* arr)
+{
+	int i;
+	for(i = 0;i<len;i++)
+	{
+		printf("%d  ",arr[i]);
+	}
+	printf("\n");
+}
