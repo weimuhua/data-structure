78988b0d68ac9660ecedacb0255395b24fceddbc
weimuhua <weimuhua0812@126.com>
2014-09-09 20:51:40 +0800

    MyStraightInsertionSort.c

---
+++ Sort/MyStraightInsertionSort.c
@@ -0,0 +1,29 @@
+#include "Sort.h"
+
+void StraightInsertionSort(int *arr);
+
+int main(int argc,char** argv)
+{
+	pri(arr);
+	printf("StraightInsertionSort:\n");
+	StraightInsertionSort(arr);
+	pri(arr);
+	return 0;
+}
+
+void StraightInsertionSort(int *arr)
+{
+	int i,j,temp;
+	for(i = 1;i<len;i++)
+	{
+		if(arr[i - 1] > arr[i])
+		{
+			temp = arr[i];
+			for(j = i - 1;j>=0 && arr[j] > temp;j--)
+			{
+				arr[j + 1] = arr[j];
+			}
+			arr[j + 1] = temp;
+		}
+	}
+}
