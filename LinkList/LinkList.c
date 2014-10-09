82a5ba66cbd7d7340ebad502f28613462dee67b5
weimuhua <weimuhua0812@126.com>
2014-10-09 12:56:41 +0800

    LinkList.c

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -6,6 +6,65 @@ typedef struct node
 	int data;
 	struct node* next;
 }node;
+
+node* createLinkList(int num);
+void deleteNode(node* head,node* toBeDeleted);
+void printList(node* head);
+
+int main(int argc,char* argv[])
+{
+	node* head = createLinkList(10);
+	printList(head);
+	deleteNode(head,head->next->next);
+	printList(head);
+	return 0;
+}
+
+void printList(node* head)
+{
+	if(head == NULL || head->next == NULL)
+		return;
+	head = head->next;
+	while(head)
+	{
+		printf("%d\t",head->data);
+		head = head->next;
+	}
+	printf("\n");
+}
+
+//O(1)复杂度删除一个结点，由用户保证链表中包含此结点
+void deleteNode(node* head,node* toBeDeleted)
+{
+	if(!head || !toBeDeleted)
+		return;
+
+	if(toBeDeleted->next != NULL)
+	{
+		node* pNext = toBeDeleted->next;
+		toBeDeleted->data = pNext->data;
+		toBeDeleted->next = pNext->next;
+		free(pNext);
+		pNext = NULL;
+	}
+	else if(head->next == toBeDeleted)
+	{
+		free(toBeDeleted);
+		toBeDeleted = NULL;
+		head->next = NULL;
+	}
+	else
+	{
+		while(head->next != toBeDeleted)
+		{
+			head = head->next;
+		}
+		head->next = NULL;
+		free(toBeDeleted);
+		toBeDeleted = NULL;
+	}
+}
+
 node* createLinkList(int num)
 {
 	node *p,*q;
@@ -24,15 +83,4 @@ node* createLinkList(int num)
 	return head;
 }
 
-int main(int argc,char* argv[])
-{
-	node* head = createLinkList(10);
-	head = head->next;
-	while(head)
-	{
-		printf("%d\t",head->data);
-		head = head->next;
-	}
-	printf("\n");
-	return 0;
-}
+
