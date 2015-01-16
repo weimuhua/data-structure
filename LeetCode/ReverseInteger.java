c7ca75005aa3d0ad94a791031ee0e7b4f261a8d8
weimuhua <weimuhua0812@126.com>
2015-01-16 12:46:09 +0800

    change:ReverseInterger.java

--- LeetCode/ReverseInteger.java
+++ LeetCode/ReverseInteger.java
@@ -4,6 +4,7 @@ public class Solution {
         while (x != 0) {
             int tail = x % 10;
             int newResult = result * 10 + tail;
+			//为防止溢出
             if ((newResult - tail) / 10 != result) {
                 return 0;
             }
